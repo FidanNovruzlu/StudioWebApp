ef44138f86b908a94c1e544b56dcd365c7d599e1
FidanNovruzlu <7l2p4qw@code.edu.az>
2023-05-22 18:48:14 +0400

    CRUD

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,12 @@
-﻿namespace StudioWebApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudioWebApp.Controllers
 {
-    public class AccountController
+    public class AccountController:Controller
     {
+       public async Task<IActionResult> Register()
+        {
+            return View();
+        }
     }
 }
